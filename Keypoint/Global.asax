0b75d7e9baaa1ba267c48c731017a4878594c076
Omer Yentur <omer_yentur@hotmail.com>
2016-06-27 18:53:20 -0400

    init

---
+++ Keypoint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Keypoint.MvcApplication" Language="C#" %>
---
+++ Keypoint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Keypoint.Startup))]
+namespace Keypoint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
