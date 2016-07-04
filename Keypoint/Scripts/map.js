08b7305bbbd785bb69aeaa56dcb8ac399fdf016c
Omer Yentur <omer_yentur@hotmail.com>
2016-07-04 18:10:00 -0400

    Menu colors updated. Contact tab updated. Google Maps added. Contact picture added.

---
+++ Keypoint/Scripts/map.js
@@ -0,0 +1,29 @@
+﻿var map1;
+
+$(document).ready(function () {
+    google.maps.visualRefresh = true;
+    initialize();
+});
+
+function initialize() {
+    var myLatlng = new google.maps.LatLng(43.693098, -79.699757);
+    var mapProp1 = {
+        center: myLatlng,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map1 = new google.maps.Map(document.getElementById("gMap1"), mapProp1);
+    //var marker1 = new google.maps.Marker({
+    //    draggable: true,
+    //    position: myLatlng,
+    //    map: map1,
+    //    title: "Keypoint Millwork"
+    //});
+    //marker1.setMap(map1);
+    //google.maps.event.addListenerOnce(map1, 'idle', function () {
+    //    google.maps.event.trigger(map1, 'resize');
+    //});
+    google.maps.event.addListenerOnce(map1, 'idle', function () {
+        google.maps.event.trigger(map1, 'resize');
+    });
+}
